1d92112aa7a6416145e143f7156821ff3381cce2
ivanopol <ivanopol777@mail.ru>
2023-11-13 04:04:49 +0500

    Added app environments

--- api/config/dependencies.php
+++ api/config/dependencies.php
@@ -2,7 +2,10 @@
 
 declare(strict_types=1);
 
-$files = glob(__DIR__ . '/common/*.php');
+$files = array_merge(
+    glob(__DIR__ . '/common/*.php') ?: [],
+    glob(__DIR__ . '/' . (getenv('APP_ENV') ?: 'prod') . '/*.php') ?: []
+);
 
 $configs = array_map(
     static function ($file) {
