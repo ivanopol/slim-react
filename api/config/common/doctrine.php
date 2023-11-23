1dbc2870a51eece41e10ef14e39420d089066f42
ivanopol <ivanopol777@mail.ru>
2023-11-23 17:37:59 +0500

    Added schema fix subscriber

--- api/config/common/doctrine.php
+++ api/config/common/doctrine.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 use App\Auth;
 use Doctrine\Common\Cache\Psr6\DoctrineProvider;
+use Doctrine\Common\EventManager;
+use Doctrine\Common\EventSubscriber;
 use Doctrine\DBAL\Types\Type;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
@@ -23,6 +25,7 @@ return [
          *     proxy_dir:string,
          *     cache_dir:?string,
          *     types:array<string,string>,
+         *     subscribers:string[],
          *     connection:array
          * } $settings
          */
@@ -46,9 +49,18 @@ return [
             }
         }
 
+        $eventManager = new EventManager();
+
+        foreach ($settings['subscribers'] as $name) {
+            /** @var EventSubscriber $subscriber */
+            $subscriber = $container->get($name);
+            $eventManager->addEventSubscriber($subscriber);
+        }
+
         return EntityManager::create(
             $settings['connection'],
-            $config
+            $config,
+            $eventManager
         );
     },
 
@@ -65,6 +77,7 @@ return [
                 'dbname' => getenv('DB_NAME'),
                 'charset' => 'utf-8'
             ],
+            'subscribers' => [],
             'metadata_dirs' => [
                 __DIR__ . '/../../src/Auth/Entity'
             ],
