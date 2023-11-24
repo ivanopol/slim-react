fd3d5ad92cd4e67473c80fc99e7754797a89b7e9
ivanopol <ivanopol777@mail.ru>
2023-11-25 00:10:20 +0500

    Migrated to frontend url generator

--- api/src/Auth/Service/JoinConfirmationSender.php
+++ api/src/Auth/Service/JoinConfirmationSender.php
@@ -6,6 +6,7 @@ namespace App\Auth\Service;
 
 use App\Auth\Entity\User\Token;
 use App\Auth\Entity\User\Email;
+use App\Frontend\FrontendUrlGenerator;
 use RuntimeException;
 use Swift_Mailer;
 use Swift_Message;
@@ -13,21 +14,21 @@ use Swift_Message;
 class JoinConfirmationSender
 {
     private Swift_Mailer $mailer;
-    private string $frontendUrl;
+    private FrontendUrlGenerator $frontend;
 
-    public function __construct(Swift_Mailer $mailer, string $frontendUrl)
+    public function __construct(Swift_Mailer $mailer, FrontendUrlGenerator $frontend)
     {
         $this->mailer = $mailer;
-        $this->frontendUrl = $frontendUrl;
+        $this->frontend = $frontend;
     }
 
     public function send(Email $email, Token $token): void
     {
         $message = (new Swift_Message('Join Confirmation'))
             ->setTo($email->getValue())
-            ->setBody($this->frontendUrl . '/join/confirm?' . http_build_query([
-                    'token' => $token->getValue(),
-                ]));
+            ->setBody($this->frontend->generate('join/confirm', [
+                'token' => $token->getValue(),
+            ]));
 
         if ($this->mailer->send($message) === 0) {
             throw new RuntimeException('Unable to send email.');
