da0f077c455bd5584176335535d8cc7e462bae27
ivanopol <ivanopol777@mail.ru>
2023-12-15 17:13:29 +0500

    Reverted reporting

--- cucumber/features/step_definitions/home.js
+++ cucumber/features/step_definitions/home.js
@@ -4,5 +4,5 @@ const { expect } = require('chai')
 Then('I see welcome block', async function () {
   await this.page.waitForSelector('[data-testid=welcome]')
   const text = await this.page.$eval('[data-testid=welcome] h1', el => el.textContent)
-  expect(text).to.eql('Auction')
+  expect(text).to.eql('Auc tion')
 })
